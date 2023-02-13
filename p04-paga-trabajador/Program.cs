5411a41715c5c0e4fb0bc42903134449fd6c4e57
jesusuaz <clashimperial117@gmail.com>
2023-02-13 11:32:54 -0600

    1.4-salario

---
+++ p04-paga-trabajador/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Calcular la paga total de un trabajador
+string nombre;
+int horas;
+float paga, pagabruta, paganeta, impuesto, tasa;
+Console.WriteLine("Calculando la paga de un trabajador...\n");
+// Entrada
+Console.Write("Nombre: ") ; nombre = Console.ReadLine();
+Console.Write("Horas trabajadas: ") ; horas = int.Parse(Console.ReadLine());
+Console.Write("Paga por hora:  ") ; paga = float.Parse(Console.ReadLine());
+tasa = 0.3f;
+// Calculo
+pagabruta = horas * paga;
+impuesto = pagabruta * tasa;
+paganeta = pagabruta - impuesto;
+// Salida
+Console.WriteLine("Resumen de pagos\n");
+Console.WriteLine($"El trabajador {nombre}, trabajo {horas} horas, con una paga de {paga} pesos la hora, se asume una tasa de impuesto de {tasa}%\n");
+Console.WriteLine($"Paga bruta: {pagabruta}");
+Console.WriteLine($"Impuessto: {impuesto}");
+Console.WriteLine($"Paga neta: {paganeta}"); 
