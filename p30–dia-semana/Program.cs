f76269f01a137ac212375bd5b6026fd932889f85
jesusuaz <clashimperial117@gmail.com>
2023-02-28 11:38:04 -0600

    29-32

---
+++ p30–dia-semana/Program.cs
@@ -0,0 +1,19 @@
+﻿// Dame un numero entre el 1 y 7 e imprime el dia de la semana
+int dia;
+
+Console.Clear();
+Console.WriteLine("Dame un numero entre 1 y 7 y saber el dia de la semana:\n");
+Console.WriteLine("Dame un numero: \n"); dia = int.Parse(Console.ReadLine());
+switch(dia) {
+
+    case 1 : Console.WriteLine("Lunes"); break;
+    case 2 : Console.WriteLine("Martes"); break;
+    case 3 : Console.WriteLine("Miercoles"); break;
+    case 4 : Console.WriteLine("Jueves"); break;
+    case 5 : Console.WriteLine("Viernes"); break;
+    case 6 : Console.WriteLine("Sabado"); break;
+    case 7 : Console.WriteLine("Domingo"); break;
+    default : Console.WriteLine("En que dia vives"); break;
+}
+
+Console.WriteLine("\n Proceso terminado...");
---
+++ p32-pago-estacionamiento/Program.cs
@@ -0,0 +1,32 @@
+﻿// Administra el pago por estacionamiento de acierdo a la zona
+
+int op;
+float tasa, pago, impuesto, total;
+
+Console.Clear();
+Console.WriteLine("Administra el pago por estacionamiento de acierdo a la zona\n"); 
+Console.WriteLine("1 - Estacionamiento Tacuba  3%");
+Console.WriteLine("2 - Estacionamiento Portales  5%");
+Console.WriteLine("3 - Estacionamiento Conqusitadores  10%");
+Console.WriteLine("4 - Estacionamiento Pajaros Cidos  15%");
+Console.WriteLine("Elige opcion: "); op = int.Parse(Console.ReadLine());
+Console.WriteLine("Pago efectuado: "); pago = float.Parse(Console.ReadLine());
+tasa = 0.0f;
+switch(op) {
+    case 1 : tasa = 0.03f;break;
+    case 2 : tasa = 0.05f;break;
+    case 3 : tasa = 0.10f;break;
+    case 4 : tasa = 0.15f;break;   
+} 
+
+impuesto = pago * tasa;
+total = pago + impuesto;
+string salida = string.Format($"Elegiste el estacionamiento {op}\n" +
+                $"Pagaste {pago} por el tiempo de uso\n" +
+                $"Corresponde un impuesto de {impuesto:n2}n" +
+                $"El pago total es de {total:n2}"); 
+                
+Console.WriteLine(salida);
+
+
+
