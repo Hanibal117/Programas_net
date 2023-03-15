84b9293a9167d1efb256f1604fae970c091f9a44
jesusuaz <clashimperial117@gmail.com>
2023-03-14 20:47:18 -0600

    actividad9

---
+++ p46–pares-ascendente/Program.cs
@@ -0,0 +1,20 @@
+﻿// Imprime números pares  de forma ascendente
+int num,c,suma;
+char resp;
+do {
+    Console.Clear();
+    Console.WriteLine("Imprime números pares ascendente\n");
+    Console.WriteLine("Hasta donde: ");
+    num = int.Parse(Console.ReadLine());
+    suma=0;
+    c=2;
+    while( c<=num ) {
+        Console.Write($"{c} ");
+        suma = suma + c;
+        c = c+2;
+    }
+    Console.WriteLine($"La suma es: {suma}");
+    Console.Write("\nDeseas continuar (S/N) ? ");
+    resp = char.ToUpper( Console.ReadLine()[0] );
+} while( resp!='N' );
+Console.WriteLine("\nGracias por utilizar este programa !");
---
+++ p47–impares-descendente/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Imprime números impares descendente
+int n,c,suma;
+char resp;
+do {
+    Console.Clear();
+    Console.WriteLine("Imprime números impares descendente\n");
+    Console.Write("Desde donde: ");
+    n = int.Parse(Console.ReadLine());
+    suma=0;
+    c = ( n%2==0 ? --n : n);
+    while( c>=1 ) {
+        Console.Write($"{c} ");
+        suma = suma + c;
+        c = c-2;
+    }
+    Console.WriteLine($"\nLa suma es {suma}");
+    Console.Write("\nDeseas continuar (S/N) ? ");
+    resp = char.ToUpper( Console.ReadLine()[0] );
+} while( resp!='N' );
+Console.WriteLine("\nGracias por utilizar este programa !");
